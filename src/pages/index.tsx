211eebf67f3c4d4fa02c4c16cade36c5853cd658
Sheba467 <lowyouone@gmail.com>
2022-03-15 21:56:00 +0200

    adding recent posts to homePage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,38 @@ import { Link, useStaticQuery, graphql } from 'gatsby';
 import Layout from '../components/layout';
 
 export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+    query GetBlogPosts {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        nodes {
+          id
+          slug
+          frontmatter {
+            title
+            description
+            date(fromNow: true)
+          }
+        }
+      }
+    }
+  `);
+
+  const posts = data.allMdx.nodes;
   return (
     <>
       <Layout>
         <h1>Hello Yanir Blog!</h1>
         <Link to="/about">About this site</Link>
+
+        <h2>Check out my recent posts</h2>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <Link to={post.slug}>{post.frontmatter.title}</Link>{' '}
+              <small>Posted {post.frontmatter.date}</small>
+            </li>
+          ))}
+        </ul>
       </Layout>
     </>
   );
